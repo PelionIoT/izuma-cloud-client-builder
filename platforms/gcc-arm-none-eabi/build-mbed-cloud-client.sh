7c22c21d763ae35e6509bf94bb426b3f52ede2e1
petedyerarm <pete.dyer@izumanetworks.com>
2024-02-12 11:26:11 +0000

    Trying to get gcc-arm-none-eabi to build.

--- platforms/gcc-arm-none-eabi/build-mbed-cloud-client.sh
+++ platforms/gcc-arm-none-eabi/build-mbed-cloud-client.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-PATH=/builder/windriver-aarch64/toolchain/gcc-arm-none-eabi/bin:$PATH
+PATH=/builder/gcc-arm-none-eabi/toolchain/gcc-arm-none-eabi/bin:$PATH
 export PATH=$PATH
-SYSROOT=/builder/windriver-aarch64/toolchain/gcc-arm-none-eabi
+SYSROOT=/builder/gcc-arm-none-eabi/toolchain/gcc-arm-none-eabi
 ARMGCC_DIR=$SYSROOT
 export ARMGCC_DIR=$ARMGCC_DIR
 CC=aarch64-none-linux-gnu-gcc
@@ -16,34 +16,48 @@ IZUMA_USE_CORES="${IZUMA_USE_CORES:-8}"
 # remove anything from a previous builder
 rm -rf /work/mbed-cloud-client-example
 # start with the original
-cp -a /work/mbed-cloud-client-example.orig /work/mbed-cloud-client-aarch64
+cp -a /work/mbed-cloud-client-example.orig /work/mbed-cloud-client-gcc-arm-none-eabi
 # we have to keep the original name b/c apparent it is referenced statically in the CMake files
-cd /work/mbed-cloud-client-aarch64
+cd /work/mbed-cloud-client-gcc-arm-none-eabi
 
 # ran during docker build - skip
 #mbed deploy
 # apply patch for aarch64
-if [ -z "${SKIP_PATCH}" ]; then
-    patch -s -p1 < ../mbed-cloud-client-example-aarch64.patch
-else
-    echo "Skipping patches"
-fi
-#python3 pal-platform/pal-platform.py deploy --target=Yocto_Generic_YoctoLinux_mbedtls generate
+#if [ -z "${SKIP_PATCH}" ]; then
+    #patch -s -p1 < ../mbed-cloud-client-example-aarch64.patch
+#else
+    #echo "Skipping patches"
+#fi
+
 if [ -e /auth/mbed_cloud_dev_credentials.c ]; then
     cp /auth/mbed_cloud_dev_credentials.c .
 else
     echo "Need to place mbed_cloud_dev_credentials.c in /auth"
     exit 1
 fi
-if [[ -z "${IZUMA_ACCESS_KEY}" ]]; then 
-    echo "Need to set IZUMA_ACCESS_KEY"
+if [ -e /auth/update_default_resources.c ]; then
+    cp /auth/update_default_resources.c .
+else
+    if [[ -z "${IZUMA_ACCESS_KEY}" ]]; then
+        manifest-dev-tool init
+    else
+        manifest-dev-tool init --access-key "${IZUMA_ACCESS_KEY}"
+    fi
+    if [ -d ".manifest-dev-tool" ]; then
+        cp -r ./.manifest-dev-tool /out/manifest-dev-tool-gcc-x86
+    else
+        echo "Cloud not find .manifest-dev-tool folder"
+    fi
+fi
+if [ -e /auth/fota_linux_ifs.cpp ]; then
+    cp /auth/fota_linux_ifs.cpp .
+else
+    echo "Need to place fota_linux_ifs.cpp in /auth"
     exit 1
-else 
-    manifest-dev-tool init --access-key ${IZUMA_ACCESS_KEY}
 fi
 
 cd __Yocto_Generic_YoctoLinux_mbedtls/
-cmake --G "Unix Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_TOOLCHAIN_FILE="./../pal-platform/Toolchain/ARMGCC/ARMGCC.cmake" -DEXTERNAL_DEFINE_FILE="./../define.txt" -- -j ${IZUMA_USE_CORES}
+cmake --G "Unix Makefiles" -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_TOOLCHAIN_FILE="./../pal-platform/Toolchain/ARMGCC/ARMGCC.cmake" -DEXTERNAL_DEFINE_FILE="./../define.txt"
 make mbedCloudClientExample.elf
 
 if [ -e __Yocto_Generic_YoctoLinux_mbedtls/Debug/mbedCloudClientExample.elf ]; then
--- platforms/gcc-arm-none-eabi/prebuild.sh
+++ platforms/gcc-arm-none-eabi/prebuild.sh
@@ -1,6 +1,5 @@
 # DOWNLOADS
 
-# aarch64 for Wind River
 if [ ! -e $ADDS/gcc-arm-10.3-2021.07-x86_64-arm-none-eabi.tar.xz ]; then
     curl https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.3-2021.07/binrel/gcc-arm-10.3-2021.07-x86_64-arm-none-eabi.tar.xz --output $ADDS/gcc-arm-10.3-2021.07-x86_64-arm-none-eabi.tar.xz
 fi
